e4903f0384f221fe8bb9d2445b9be527409c6577
Harrum Noor <i140065@nu.edu.pk>
2018-06-01 19:58:01 +0500

    initial commit

---
+++ react-quiz/src/DummyData/quizQuestions.js
@@ -0,0 +1,69 @@
+var quizQuestions = [
+  {
+      question: "Pick the definition of despicable.",
+      answers: [
+          {
+              type: "True",
+              content: "Deserving hatred and contempt."
+          },
+          {
+              type: "False",
+              content: "Deserving love and affection."
+          }
+      ]
+  },
+  {
+      question: "Pick the definition of amiable.",
+      answers: [
+          {
+              type: "True",
+              content: "Having or displaying a friendly and pleasant manner."
+          },
+          {
+              type: "False",
+              content: "Having or displaying bad manners."
+          }
+      ]
+  },
+  {
+      question: "Pick the definition of wrath.",
+      answers: [
+          {
+              type: "True",
+              content: "Extreme anger."
+          },
+          {
+              type: "False",
+              content: "Garage in British English."
+          }
+      ]
+  },
+  {
+      question: "Pick the definition of archaic.",
+      answers: [
+          {
+              type: "True",
+              content: "Very old or old-fashioned."
+          },
+          {
+              type: "False",
+              content: "Trendy."
+          }
+      ]
+  },
+  {
+      question: "Pick the definition of antediluvian.",
+      answers: [
+          {
+              type: "True",
+              content: "Belonging to the time before the biblical Flood."
+          },
+          {
+              type: "False",
+              content: "A British era."
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
